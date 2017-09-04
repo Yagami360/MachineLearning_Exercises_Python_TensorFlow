1ab10bfbfb7a9aa68360b08a32c24cc9f3e6e7eb
Yagami <y034112@gmail.com>
2017-09-04 20:26:13 +0900

    Add files via upload

--- Processing_TensorFlow/main2.py
+++ Processing_TensorFlow/main2.py
@@ -20,29 +20,77 @@ def main():
     """
     print("Enter main()")
 
-    #======================================================================
-    # テンソルを作成
-    #======================================================================
-    #hello_tsr = tf.constant( "Hello, TensorFlow!" )
 
+    #======================================================================
+    # テンソルの設定
+    #======================================================================
+    #--------------------------------------
     # 固定のテンソルを作成
-    zero_tsr = tf.zeros( [3, 2] )
+    #--------------------------------------
+    # tf.zeros(...) : 全ての要素が 0 からなる Tensor を作成する
+    zeros_tsr = tf.zeros( [3, 2] )
     
     # Session を生成せず、そのまま Tensor を print()
-    print( zero_tsr )                   # <出力> 
-                                        # Tensor("zeros:0", shape=(3, 2), dtype=float32)
-                                        # Tensor オブジェクトが出力されている。
-                                        # これは session を実行していないため。
+    print(  " tf.zeros(...) の Tensor 型 : \n", zeros_tsr )   # <出力> 
+                                                              # Tensor("zeros:0", shape=(3, 2), dtype=float32)
+                                                              # Tensor オブジェクトが出力されている。
+                                                              # これは session を実行していないため。
 
     # Session を生成＆run() して、Tensor を print()
     session = tf.Session()
-    print( session.run( zero_tsr ) )    # <出力>
+    print( session.run( zeros_tsr ) )   # <出力>
                                         # [[ 0.  0.]
                                         #  [ 0.  0.]
                                         #  [ 0.  0.]
-                                        # 全ての要素が 0 からなるテンソル（この場合、２次配列）が出力されている。
+                                        # 全ての要素が 0 からなる Tensor（この場合、２次配列）が出力されている。
     
+    session.close()
 
+
+    # tf.ones(...) : 全ての要素が 1 からなる Tensor を作成する
+    ones_tsr = tf.ones( [3, 2] )
+    # Session を生成せず、そのまま Tensor を print()
+    print(  " tf.ones(...) の Tensor 型 : \n", ones_tsr )
+
+    # Session を生成＆run() して、Tensor を print()
+    session = tf.Session()
+    print( "tf.ones(...) の value : ", session.run( ones_tsr ) )    # <出力>
+                                                                    # [[ 1.  1.]
+                                                                    #  [ 1.  1.]
+                                                                    #  [ 1.  1.]
+    session.close()
+
+
+    # tf.fill(...) : 指定した定数で埋められた Tensor を作成する。
+    filled_tsr = tf.fill( [3, 2], "Const" )
+    print( "tf.fill(...) の Tensor 型 : ", filled_tsr )
+
+    session = tf.Session()
+    print( "tf.fill(...) の value : ", session.run( filled_tsr ) )
+    session.close()
+
+
+    # tf.constant(...) : 指定した既存の定数から Tensor を作成する。
+    const_tsr = tf.constant( [1,2,3] )
+    print( "tf.constant(...) の Tensor 型 : ", const_tsr )
+
+    session = tf.Session()
+    print( "tf.constant(...) の value : ", session.run( const_tsr ) )
+    session.close()
+
+
+    #--------------------------------------
+    # シーケンステンソルを作成
+    #--------------------------------------
+    # tf.linspace(...) : 
+    liner_tsr = tf.linspace( start = 0.0, stop = 1.0, num = 3 )
+    print( "tf.linspace(...) の Tensor 型 : ", liner_tsr )
+    
+    session = tf.Session()
+    print( "tf.linspace(...) の value : ", session.run( liner_tsr ) )
+    session.close()
+
+    # 
     #======================================================================
     # 変数とプレースホルダを設定
     #======================================================================
