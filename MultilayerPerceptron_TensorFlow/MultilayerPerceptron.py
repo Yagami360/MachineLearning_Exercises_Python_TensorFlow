a7689f61a6cfa8df7c5e5c9cce36a1df690b515d
Yagami <y034112@gmail.com>
2017-10-14 05:34:44 +0900

    Add files via upload

--- MultilayerPerceptron_TensorFlow/MultilayerPerceptron.py
+++ MultilayerPerceptron_TensorFlow/MultilayerPerceptron.py
@@ -26,16 +26,99 @@ class MultilayerPerceptron( NeuralNetworkBase ):
     ----------------------------------------------------------------------------------------------------
 
     [protedted] protedted な使用法を想定 
+        _n_inputLayer : int
+            入力層のノード数
+        _n_hideenLayers : shape = [h1,h2,h3,...] 
+            h1 : 1 つ目の隠れ層のユニット数、h2 : 2 つ目の隠れ層のユニット数、...
+        _n_outputLayer : int
+            出力層のノード数
 
     [public] public アクセス可能なインスタスンス変数には, 便宜上変数名の最後にアンダースコア _ を付ける.
+        _X_holder : placeholder
+            入力層にデータを供給するための placeholder
 
     [private] 変数名の前にダブルアンダースコア __ を付ける（Pythonルール）
 
     """
 
-    def __init__( self ):
+    def __init__( self, n_inputLayer = 1, n_hideenLayers = [1,1,1], n_outputLayer = 1 ):
         """
         コンストラクタ（厳密にはイニシャライザ）
         """
+        # メンバ変数の初期化
+        self._n_inputLayer = n_inputLayer
+        self._n_hideenLayers = n_hideenLayers
+        self._n_outputLayer = n_outputLayer
+
+        # shape は、入力層の次元 self._n_inputLayer に対応させる。
+        self._X_holder = tf.placeholder( tf.float32, shape = [None, self._n_inputLayer] )
+        
+        return
+    
+    def print( self, str ):
+        print( "----------------------------------" )
+        print( "MultilayerPerceptron" )
+        print( self )
+        print( str )
+        print( "_n_inputLayer : ", self._n_inputLayer )
+        print( "_n_hideenLayers : ", self._n_hideenLayers )
+        print( "_n_outputLayer : ", self._n_outputLayer )
+        print( "----------------------------------" )
+        return
+
+    @classmethod
+    def models( self ):
+        """
+        モデルの定義を行う。
+        """
+
+        return self
+
+    @classmethod
+    def loss( self ):
+        """
+        損失関数の定義を行う。
+        """
+
+        return self
+
+    @classmethod
+    def optimizer( self ):
+        """
+        モデルの最適化アルゴリズムの設定を行う。
+        """
+
+        return self
+
+    def fit( self, X_train, y_train ):
+        """
+        指定されたトレーニングデータで、モデルの fitting 処理を行う。
+
+        [Input]
+            X_train : numpy.ndarray ( shape = [n_samples, n_features] )
+                トレーニングデータ（特徴行列）
+            
+            y_train : numpy.ndarray ( shape = [n_samples] )
+                レーニングデータ用のクラスラベル（教師データ）のリスト
+
+        [Output]
+            self : 自身のオブジェクト
+        """
+        return self
+
+
+    def predict( self, X_features ):
+        """
+        fitting 処理したモデルで、推定を行い、予想値を返す。（抽象メソッド）
+
+        [Input]
+            X_features : numpy.ndarry ( shape = [n_samples, n_features] )
+                予想したい特徴行列
+
+        [Output]
+            results : numpy.ndarry ( shape = [n_samples] )
+                予想結果（分類モデルの場合は、クラスラベル）
+        """
 
         return
+
--- MultilayerPerceptron_TensorFlow/NeuralNetworkBase.py
+++ MultilayerPerceptron_TensorFlow/NeuralNetworkBase.py
@@ -8,11 +8,16 @@
                : 
 """
 
+from abc import ABCMeta, abstractmethod     # 抽象クラスを作成するための ABC クラス
+
+import numpy
+
 # TensorFlow ライブラリ
 import tensorflow as tf
 from tensorflow.python.framework import ops
 
-class NeuralNetworkBase(object):
+
+class NeuralNetworkBase( metaclass=ABCMeta ):
     """
     ニューラルネットワークの基底クラス（自作クラス）
     TensorFlow ライブラリを使用
@@ -23,6 +28,10 @@ class NeuralNetworkBase(object):
     ----------------------------------------------------------------------------------------------------
 
     [protedted] protedted な使用法を想定 
+        _n_inputLayer : int
+            入力層のノード数
+        _n_outputLayer : int
+            出力層のノード数
 
     [public] public アクセス可能なインスタスンス変数には, 便宜上変数名の最後にアンダースコア _ を付ける.
 
@@ -34,6 +43,77 @@ class NeuralNetworkBase(object):
         """
         コンストラクタ（厳密にはイニシャライザ）
         """
+        # メンバ変数の初期化
+        self._n_inputLayer = 1
+        self._n_outputLayer = 1
 
         return
 
+    
+    def print( self, str ):
+        print( "NeuralNetworkBase" )
+        print( self )
+        print( str )
+
+        return
+
+
+    @classmethod
+    @abstractmethod
+    def models( self ):
+        """
+        モデルの定義を行う。（抽象メソッド）
+        """
+        return self
+
+    
+    @classmethod
+    @abstractmethod
+    def loss( self ):
+        """
+        損失関数（誤差関数、コスト関数）の定義を行う。（抽象メソッド）
+        """
+        return self
+
+    
+    @classmethod
+    @abstractmethod
+    def optimizer( self ):
+        """
+        モデルの最適化アルゴリズムの設定を行う。（抽象メソッド）
+        """
+        return self
+
+    
+    @abstractmethod
+    def fit( self, X_train, y_train ):
+        """
+        指定されたトレーニングデータで、モデルの fitting 処理を行う。（抽象メソッド）
+
+        [Input]
+            X_train : numpy.ndarray ( shape = [n_samples, n_features] )
+                トレーニングデータ（特徴行列）
+            
+            y_train : numpy.ndarray ( shape = [n_samples] )
+                レーニングデータ用のクラスラベル（教師データ）のリスト
+
+        [Output]
+            self : 自身のオブジェクト
+        """
+        return self
+
+
+    @abstractmethod
+    def predict( self, X_features ):
+        """
+        fitting 処理したモデルで、推定を行い、予想値を返す。（抽象メソッド）
+
+        [Input]
+            X_features : numpy.ndarry ( shape = [n_samples, n_features] )
+                予想したい特徴行列
+
+        [Output]
+            results : numpy.ndaary ( shape = [n_samples] )
+                予想結果（分類モデルの場合は、クラスラベル）
+        """
+        return
